073f8c4e0fa4c562bb7b2f3a9dad3727b765f5c0
jasondong <dongjiasheng@gowining.com>
2025-11-13 10:19:35 +0800

    兼容dashscope qwen model

--- hipporag_lite/embedding_model/__init__.py
+++ hipporag_lite/embedding_model/__init__.py
@@ -1,5 +1,6 @@
 from .base import EmbeddingConfig, BaseEmbeddingModel
 from .Qwen3 import Qwen3EmbeddingModel
+from .Qwen import QwenEmbeddingModel
 
 from ..utils.logging_utils import get_logger
 
@@ -9,4 +10,6 @@ logger = get_logger(__name__)
 def _get_embedding_model_class(embedding_model_name: str = "nvidia/NV-Embed-v2"):
     if "Qwen/Qwen3-Embedding-8B" in embedding_model_name:
         return Qwen3EmbeddingModel
+    elif 'text-embedding' in embedding_model_name:
+        return QwenEmbeddingModel
     assert False, f"Unknown embedding model name: {embedding_model_name}"
